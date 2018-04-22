93d519481c38f3f619dae350d39ca7ae4da411d9
DanJRaymond <34133006+DanJRaymond@users.noreply.github.com>
2018-04-22 22:44:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// document.addEventListener("DOMContentLoaded", onLoad);
+//
+// var tog1 = 0;
+// var bod;
+// var li;
+//
+//
+// function onLoad () {
+//   var b1 = document.getElementsByClassName("button1")[0];
+//   b1.addEventListener("click", onClick);
+// }
+//
+// function onClick () {
+//
+//
+//   if (tog1 == 0) {
+//   clickOn();
+// } else if (tog1 == 1) {
+//   clickOff();
+//   }
+// }
+//
+// function clickOn () {
+//   bod = document.getElementsByClassName("button1")[0];
+//   li = document.createElement("div");
+//   var text = document.createTextNode("esgeg");
+//
+//   bod.appendChild(li);
+//   li.appendChild(text);
+//   tog1 = 1;
+// }
+//
+// function clickOff () {
+//   bod.removeChild(li);
+//   tog1 = 0;
+// }
